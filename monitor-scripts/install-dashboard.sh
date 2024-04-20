7bdb7b46347c357bc2dbb779dae5edd75b4c77fc
sic <31908995+sicXnull@users.noreply.github.com>
2024-04-20 14:55:19 -0400

    Update install-dashboard.sh

--- monitor-scripts/install-dashboard.sh
+++ monitor-scripts/install-dashboard.sh
@@ -7,5 +7,5 @@ sleep 5
 systemctl start nginx
 systemctl enable nginx
 
-systemctl start php7.3-fpm.service
-systemctl enable php7.3-fpm.service
+systemctl start php8.3-fpm.service
+systemctl enable php8.3-fpm.service
