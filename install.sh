eb043ee94f6c6af6148a5d2dc9b0d9cb265cdc5e
sic <31908995+sicXnull@users.noreply.github.com>
2024-04-20 14:33:20 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,16 +9,16 @@ fi
 
 if test -d /var/dashboard; then
   echo 'Dashboard already installed, running an update...'
-  wget https://raw.githubusercontent.com/Panther-X/PantherDashboard/${BRANCH}/update.sh -O - | sudo bash
+  wget https://raw.githubusercontent.com/sicXnull/PantherDashboard/${BRANCH}/update.sh -O - | sudo bash
 else
   if id -nG admin | grep -qw "sudo"; then
     if test -f /var/dashboard/commit-hash; then
       VER=`cat /var/dashboard/commit-hash`
-      wget https://codeload.github.com/Panther-X/PantherDashboard/tar.gz/${VER} -O /tmp/latest.tar.gz
+      wget https://codeload.github.com/sicXnull/PantherDashboard/tar.gz/${VER} -O /tmp/latest.tar.gz
     else
-      wget https://raw.githubusercontent.com/Panther-X/PantherDashboard/${BRANCH}/version -O /tmp/dashboard_latest_ver
+      wget https://raw.githubusercontent.com/sicXnull/PantherDashboard/${BRANCH}/version -O /tmp/dashboard_latest_ver
       VER=`cat /tmp/dashboard_latest_ver`
-      wget https://codeload.github.com/Panther-X/PantherDashboard/tar.gz/refs/tags/${VER} -O /tmp/latest.tar.gz
+      wget https://codeload.github.com/sicXnull/PantherDashboard/tar.gz/refs/tags/${VER} -O /tmp/latest.tar.gz
     fi
     cd /tmp
     if test -s latest.tar.gz; then
