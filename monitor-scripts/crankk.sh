4fe1a5c49b13c6b4fa9f0f06f2ef0d86d8fad640
sic <31908995+sicXnull@users.noreply.github.com>
2024-04-21 14:15:36 -0400

    Update crankk.sh

--- monitor-scripts/crankk.sh
+++ monitor-scripts/crankk.sh
@@ -7,13 +7,15 @@ if docker ps -a --format '{{.Names}}' | grep -q "^crankk$"; then
     # Check if the Docker container crankk is running
     if docker ps --format '{{.Names}}' | grep -q "^crankk$"; then
         echo "Container 'crankk' is running. No action needed."
+        exit 0
     else
         echo "Container 'crankk' is stopped. Removing container..."
-        # Remove the stopped container named 'crankk'
         docker rm crankk
+        exit $?
     fi
 else
     echo "Container 'crankk' does not exist. Creating and starting container..."
     docker run --name crankk --network host --privileged --restart always \
                -v /data:/crankk_data -v /:/host crankkster/crankk
+    exit $?
 fi
