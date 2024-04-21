a987bd14a804d59571800d32caa0f1bfa66ee397
sic <31908995+sicXnull@users.noreply.github.com>
2024-04-21 14:32:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,19 +7,17 @@ else
   BRANCH='stock-firmware'
 fi
 
-if test -d /var/dashboard; then
-  echo 'Dashboard already installed, running an update...'
-  wget https://raw.githubusercontent.com/sicXnull/PantherDashboard/${BRANCH}/update.sh -O - | sudo bash
-else
-  if id -nG admin | grep -qw "sudo"; then
+if id -nG admin | grep -qw "sudo"; then
     LATEST_COMMIT=$(curl -s https://api.github.com/repos/sicXnull/PantherDashboard/git/refs/heads/${BRANCH} | jq -r '.object.sha')
     wget https://github.com/sicXnull/PantherDashboard/archive/${LATEST_COMMIT}.tar.gz -O /tmp/latest.tar.gz
     cd /tmp
     if test -s latest.tar.gz; then
       rm -rf /tmp/PantherDashboard-*
+	  
+	  mkdir -p /tmp/PantherDashboard
     
-      tar -xzf latest.tar.gz
-      cd PantherDashboard-${VER}
+      tar -xzf latest.tar.gz -C /tmp/PantherDashboard --strip-components=1
+      cd PantherDashboard
       apt-get update
       apt-get --assume-yes install nginx php-fpm php7.3-fpm ngrep gawk php-cli logrotate netcat jq
 
@@ -54,8 +52,15 @@ else
         sh -c 'echo www-data ALL=\(ALL\) NOPASSWD: /etc/monitor-scripts/first-load.sh >> /etc/sudoers.d/www-data'
       fi
 
-      openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
-      openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=CN/ST=Panther/L=Panther/O=Panther/CN=localhost" -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+      # Check if the DH parameters file already exists
+	if [ ! -f /etc/ssl/certs/dhparam.pem ]; then
+		openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+	fi
+
+	# Check if the self-signed certificate and key files already exist
+	if [ ! -f /etc/ssl/certs/nginx-selfsigned.crt ] || [ ! -f /etc/ssl/private/nginx-selfsigned.key ]; then
+		openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 -subj "/C=CN/ST=Panther/L=Panther/O=Panther/CN=localhost" -keyout /etc/ssl/private/nginx-selfsigned.key -out /etc/ssl/certs/nginx-selfsigned.crt
+	fi
     
       cp systemd/* /etc/systemd/system/
 
@@ -73,30 +78,37 @@ else
       chown root:www-data /var/dashboard
       chmod 775 /var/dashboard
       chmod 775 /var/dashboard/logs
+	  systemctl daemon-reload
+	  
+	  
+      if [ condition ]; then
+          bash /etc/monitor-scripts/pantherx-ver-check.sh
+          FILES="systemd/*.timer"
+          for f in $FILES;
+          do
+             name=$(echo $f | sed 's/.timer//' | sed 's/systemd\///')
+             systemctl start $name.timer
+             systemctl enable $name.timer
+             systemctl start $name.service
+          done
 
-      bash /etc/monitor-scripts/pantherx-ver-check.sh
-      FILES="systemd/*.timer"
-      for f in $FILES;
-      do
-         name=$(echo $f | sed 's/.timer//' | sed 's/systemd\///')
-         systemctl start $name.timer
-         systemctl enable $name.timer
-         systemctl start $name.service
-      done
+          systemctl daemon-reload
+          systemctl enable packet-forwarder-sniffer.service
+          systemctl start packet-forwarder-sniffer.service
 
-      systemctl daemon-reload
-      systemctl enable packet-forwarder-sniffer.service
-      systemctl start packet-forwarder-sniffer.service
+          systemctl enable nginx
+          systemctl restart nginx
+          bash /etc/monitor-scripts/pubkeys.sh
 
-      systemctl enable nginx
-      systemctl restart nginx
-      bash /etc/monitor-scripts/pubkeys.sh
-
-      echo 'Success.'
+          echo 'Success.'
+      elif [ another_condition ]; then
+          echo 'No installation archive found.  No changes made.'
+      else
+          echo 'Error checking if admin user exists.  No changes made.'
+      fi
     else
       echo 'No installation archive found.  No changes made.'
     fi
-  else
-    echo 'Error checking if admin user exists.  No changes made.';
-  fi
+else
+    echo 'Error checking if admin user exists.  No changes made.'
 fi
