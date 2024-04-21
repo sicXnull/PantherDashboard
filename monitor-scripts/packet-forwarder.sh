3a58b00303fba268185759322ceaaeeb3a6b2d5d
sic <31908995+sicXnull@users.noreply.github.com>
2024-04-20 20:08:31 -0400

    Update packet-forwarder.sh

--- monitor-scripts/packet-forwarder.sh
+++ monitor-scripts/packet-forwarder.sh
@@ -2,9 +2,7 @@
 
 # Check if the Docker container "crankk-pktfwd" is running
 if ! docker ps | grep -q "crankk-pktfwd"; then
-    # If the container is not running, make the file blank
     > /var/dashboard/statuses/packet-forwarder
 else
-    # If the container is running, copy the log to the status file
-    sudo cat /var/log/packet-forwarder/packet_forwarder.log > /var/dashboard/statuses/packet-forwarder
+    echo "enabled" > /var/dashboard/statuses/packet-forwarder
 fi
